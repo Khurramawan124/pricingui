02ccdfd3a42da5d59b70fc5396029800821d4696
khurram awan <khurramawan124@gmail.com>
2023-02-01 23:47:06 +0500

    changes with icon

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -6,7 +6,7 @@ export const  ListItem = (props: StackProps)=>{
     const {children, ...rest} = props;
     return(
       <HStack as= 'li' spacing = '20px' {...rest}  alignItems="start">
-        <Icon as ={CheckIcon} w='22px' h='22px'/>
+        <Icon as ={CheckIcon} w={['12px','12px','22px']} h={['9px','9px','22px']}/>
         <Text textAlign={["left","left","center"]}>{children}</Text>
       </HStack>
     );
